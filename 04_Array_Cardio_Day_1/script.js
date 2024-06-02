963eec2f57280742716b700ce080fe21665b221e
sryung <sryung1225@gmail.com>
2024-06-02 16:59:25 +0900

    [4][init] 데이터를 별도의 json 파일로 분리 후 fetch

--- 04_Array_Cardio_Day_1/script.js
+++ 04_Array_Cardio_Day_1/script.js
@@ -1,69 +1,18 @@
-// Get your shorts on - this is an array workout!
-// ## Array Cardio Day 1
+// 데이터를 json으로 분리하고 따로 데이터 가져오기 수행
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const inventors = data.inventors;
+    const people = data.people;
 
-// Some data we can work with
+    born1500s(inventors);
+  })
+  .catch((error) => console.error("못 가져 왔담"));
 
-const inventors = [
-  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
-  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
-  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
-  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
-  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
-  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
-  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
-  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
-  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
-  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
-  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
-  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
-];
-
-const people = [
-  "Bernhard, Sandra",
-  "Bethea, Erin",
-  "Becker, Carl",
-  "Bentsen, Lloyd",
-  "Beckett, Samuel",
-  "Blake, William",
-  "Berger, Ric",
-  "Beddoes, Mick",
-  "Beethoven, Ludwig",
-  "Belloc, Hilaire",
-  "Begin, Menachem",
-  "Bellow, Saul",
-  "Benchley, Robert",
-  "Blair, Robert",
-  "Benenson, Peter",
-  "Benjamin, Walter",
-  "Berlin, Irving",
-  "Benn, Tony",
-  "Benson, Leana",
-  "Bent, Silas",
-  "Berle, Milton",
-  "Berry, Halle",
-  "Biko, Steve",
-  "Beck, Glenn",
-  "Bergman, Ingmar",
-  "Black, Elk",
-  "Berio, Luciano",
-  "Berne, Eric",
-  "Berra, Yogi",
-  "Berry, Wendell",
-  "Bevan, Aneurin",
-  "Ben-Gurion, David",
-  "Bevel, Ken",
-  "Biden, Joseph",
-  "Bennington, Chester",
-  "Bierce, Ambrose",
-  "Billings, Josh",
-  "Birrell, Augustine",
-  "Blair, Tony",
-  "Beecher, Henry",
-  "Biondo, Frank",
-];
-
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
+// 1. 1500년대에 태어난 inventors만 나열하기
+function born1500s(inventors) {
+  console.log("하이", inventors.length);
+}
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
