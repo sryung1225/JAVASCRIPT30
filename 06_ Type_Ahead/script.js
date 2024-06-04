bd83af2bd6de81e63dbe8d58d4020bd5b86b7db9
sryung <sryung1225@gmail.com>
2024-06-04 23:36:53 +0900

    [6][feature] json 파일 fetch
    - [4]에서와 다른 점은 전개연산자+push를 통해 fetch 함수 외부의 전역 상수를 사용한 것

--- 06_ Type_Ahead/script.js
+++ 06_ Type_Ahead/script.js
@@ -1,3 +1,14 @@
+// 검색어에 해당하는 데이터 자동 완성하기
+// -
 // -----------------------------------------------------
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => {
+    cities.push(...data);
+    console.log(cities); // ? (1000) [{…}, ...]
+  })
+  .catch((error) => console.error("못 가져 왔담"));
