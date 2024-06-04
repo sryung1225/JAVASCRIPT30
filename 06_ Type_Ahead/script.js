72486abc4da3fda177481b4ac9ab59680a6e9946
sryung <sryung1225@gmail.com>
2024-06-05 00:00:22 +0900

    [6][feature] 검색어가 포함된 데이터 목록 자동 완성
    - 정규표현식 활용
    - innerHTML을 활용한 문자열 html에 직접 삽입

--- 06_ Type_Ahead/script.js
+++ 06_ Type_Ahead/script.js
@@ -1,5 +1,7 @@
 // 검색어에 해당하는 데이터 자동 완성하기
-// -
+// - json 파일을 이용하여 데이터 fetch
+// - 검색어가 포함되는 데이터를 찾는 수단으로 정규표현식 활용
+// - 검색어와 일치하는 부분을 표시하는 수단으로 정규표현식 활용
 // -----------------------------------------------------
 const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
@@ -9,6 +11,50 @@ fetch(endpoint)
   .then((response) => response.json())
   .then((data) => {
     cities.push(...data);
-    console.log(cities); // ? (1000) [{…}, ...]
+    // console.log(cities); // ? (1000) [{…}, ...]
   })
   .catch((error) => console.error("못 가져 왔담"));
+
+function findMatches(wordToMatch, cities) {
+  // 검색어(wordToMatch)가 city 혹은 state에 존재하는 경우를 필터링
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    // g: global. 문자열의 전체를 탐색
+    // i: ignore case. 대/소문자 구분 없이 탐색
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function displayMatches() {
+  // console.log(this.value); // 검색어
+
+  // findMatches를 통해 나온 결과를 화면에 렌더링
+  const matchArray = findMatches(this.value, cities);
+  const regex = new RegExp(this.value, "gi");
+  const html = matchArray
+    .map((place) => {
+      // 검색어와 일치하는 부분을 강조하기 위해 i.hl로 감싼 요소로 replace
+      const cityName = place.city.replace(
+        regex,
+        `<i class="hl">${this.value}</i>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<i class="hl">${this.value}</i>`
+      );
+      // li로 `cityName, stateName`를 감싸서 출력
+      return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+    </li>
+  `;
+    })
+    .join(""); // matchArray.map() 한 결과는 배열이므로 join("")을 통해 문자열화
+  suggestions.innerHTML = html; // suggestions 내부에 html 요소로 삽입
+}
+
+const searchInput = document.querySelector("input.search");
+const suggestions = document.querySelector("ul.suggestions");
+
+// searchInput.addEventListener("change", displayMatches); // input 입력 후 focus 벗어날 때에만 포착
+searchInput.addEventListener("keyup", displayMatches); // input 입력 모든 순간 포착
