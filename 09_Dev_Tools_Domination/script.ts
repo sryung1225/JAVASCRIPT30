01d633e68b1ed5dacc86290ea6fa5dae393c65ee
sryung <sryung1225@gmail.com>
2024-06-07 21:58:03 +0900

    [9][feature] typescript로 변환 - console.groupEnd()에서 인수는 선택사항이라 타입 에러가 발생. 인수를 생략해도 동일 동작하기 때문에 생략해서 타입 에러 해결

--- 09_Dev_Tools_Domination/script.ts
+++ 09_Dev_Tools_Domination/script.ts
@@ -1,4 +1,3 @@
-"use strict";
 // 개발자 도구 명령어 알아보기
 // - dir: DOM 요소의 속성을 트리 구조로 출력
 // - table: 데이터를 테이블 형식으로 출력
@@ -9,49 +8,73 @@
 // - time : 타이머 시작
 // - timeEnd : 타이머 종료 & 소요된 시간 출력
 // -----------------------------------------------------
+
 function makeGreen() {
-    const p = document.querySelector('p');
-    p.style.color = '#BADA55';
-    p.style.fontSize = '50px';
+  const p = document.querySelector('p') as HTMLDivElement;
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
 }
+
 // Regular
 console.log('hello');
 // ? hello
+
 // Interpolated
 console.log('Hello I an a %s string!', '💩');
 // ? Hello I an a 💩 string!
+
 // Styled
-console.log('%c 얏호', 'font-size:50px; background-color:gray; text-shadow: 5px 5px 0 white');
+console.log(
+  '%c 얏호',
+  'font-size:50px; background-color:gray; text-shadow: 5px 5px 0 white',
+);
+
 // Warning!
 console.warn('OH NOOO');
 // ? ⚠️ OH NOOO
+
 // Error
 console.error('Shit!');
 // ? 🚨 Shit!
+
 // Info
 console.info('Crocodiles eat 3-4 people per year');
 // ? Crocodiles eat 3-4 people per year
+
 // Testing
-const p = document.querySelector('p');
+const p = document.querySelector('p') as HTMLDivElement;
 console.assert(p.classList.contains('ouch'), 'That is wrong!');
 // ? 🚨 어설션에 실패함: That is wrong!
+
 // Clearing
 console.clear();
 // ? 콘솔 삭제됨
+
 // Viewing DOM Elements
 console.log(p);
 console.dir(p);
-const dogs = [
-    { name: 'Snickers', age: 2 },
-    { name: 'hugo', age: 8 },
+// ? p
+// accessKey: ""
+// align: ""
+// ariaAtomic: null
+// ...
+
+interface Dog {
+  name: string;
+  age: number;
+}
+const dogs: Dog[] = [
+  { name: 'Snickers', age: 2 },
+  { name: 'hugo', age: 8 },
 ];
+
 // Grouping together
 dogs.forEach((dog) => {
-    console.groupCollapsed(`${dog.name}`);
-    console.log(`This is ${dog.name}`);
-    console.log(`${dog.name} is ${dog.age} years old`);
-    console.log(`${dog.name} is ${dog.age * 7} dog years old`);
-    console.groupEnd();
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd();
 });
 // ? Snickers
 // This is Snickers
@@ -61,6 +84,7 @@ dogs.forEach((dog) => {
 // This is hugo
 // hugo is 2 years old
 // hugo is 14 dog years old
+
 // Counting
 console.count('Wes'); // ? Wes: 1
 console.count('Wes'); // ? Wes: 2
@@ -68,16 +92,18 @@ console.count('Steve'); // ? Steve: 1
 console.count('Steve'); // ? Steve: 2
 console.count('Wes'); // ? Wes: 3
 console.count('Steve'); // ? Steve: 3
+
 // Timing
 console.time('fetching data');
 fetch('https://api.github.com/users/wesbos')
-    .then((data) => data.json())
-    .then((data) => {
+  .then((data) => data.json())
+  .then((data) => {
     console.timeEnd('fetching data');
     console.log(data);
-});
+  });
 // ? fetching data: 38.925048828125 ms
 // ? {login: 'wesbos', id: 176013, node_id: 'MDQ6VXNlcjE3NjAxMw==', avatar_url: 'https://avatars.githubusercontent.com/u/176013?v=4', gravatar_id: '', …}
+
 console.table(dogs);
 // dogs에 대한 테이블 출력
 // 위에서 진행된 fetching에서 시간 소모가 있기 때문에 테이블이 우선 출력됨
