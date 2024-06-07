2e191d0991ab3d3d1fa63ed4eb1a4139560354c3
sryung <sryung1225@gmail.com>
2024-06-07 21:36:22 +0900

    [9][init] 초기 셋팅 - html + script

---
+++ 09_Dev_Tools_Domination/script.js
@@ -0,0 +1,41 @@
+// 그림판 만들기
+// - 반응형 캔버스 크기
+// - 마우스 좌표를 연속적으로 저장하면서 그림 그리기
+// - hsl 색상값을 활용하여 무지개색으로 붓 색 변화시키기 (hue: 0 ~ 360)
+// - 붓 크기 조절 (1 -> 100 -> 1 -> ...)
+// -----------------------------------------------------
+
+const dogs = [
+  { name: 'Snickers', age: 2 },
+  { name: 'hugo', age: 8 },
+];
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
