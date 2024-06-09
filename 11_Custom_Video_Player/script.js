959aa870a32969a398987562e3cc4c2f0e4f04d2
sryung <sryung1225@gmail.com>
2024-06-09 10:03:23 +0900

    [11][feature] 비디오 앞/뒤 구간 스킵

--- 11_Custom_Video_Player/script.js
+++ 11_Custom_Video_Player/script.js
@@ -4,6 +4,7 @@
 // - 재생 진행 바 표시
 // - 재생 진행 바 클릭 시 해당 구간으로 점프
 // - 재생 진행 바 드래그 시 해당 구간으로 이동
+// - 앞/뒤 스킵 버튼 누르면 현재 기준으로 구간 이동
 // -----------------------------------------------------
 
 // 요소들 선언
@@ -12,6 +13,7 @@ const video = player.querySelector('.viewer');
 const toggle = player.querySelector('.toggle');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
+const skipButtons = player.querySelectorAll('[data-skip]');
 
 // 1. 비디오 재생/정지 토글
 function togglePlay() {
@@ -47,3 +49,10 @@ progress.addEventListener('click', scrub); // 클릭 구간으로 점프
 progress.addEventListener('mousedown', () => (mousedown = true));
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); // 드래그 인식
 progress.addEventListener('mouseup', () => (mousedown = false));
+
+// 3. 비디오 앞 뒤 구간 스킵
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+skipButtons.forEach((button) => button.addEventListener('click', skip));
