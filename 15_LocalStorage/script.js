e9d357a60800410cb19a5bbefe47ac510a79a0f8
sryung <sryung1225@gmail.com>
2024-06-15 16:00:09 +0900

    [15][feature] 리스트 아이템 화면 렌더링

--- 15_LocalStorage/script.js
+++ 15_LocalStorage/script.js
@@ -1,3 +1,22 @@
+// 투두 리스트 만들기
+// - 리스트 아이템 배열 화면에 렌더링
+// -----------------------------------------------------
+
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = ['숨쉬기'];
+
+// 리스트 화면 렌더링
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+      <li>
+        ${plate}
+      </li>
+    `;
+    })
+    .join('');
+}
+
+populateList(items, itemsList);
