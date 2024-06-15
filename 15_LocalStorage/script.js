40af7ffb99862abbc8a6e0bd0d3988a48acf8d40
sryung <sryung1225@gmail.com>
2024-06-15 16:04:04 +0900

    [15][feature] 리스트에 신규 아이템 추가

--- 15_LocalStorage/script.js
+++ 15_LocalStorage/script.js
@@ -1,10 +1,21 @@
 // 투두 리스트 만들기
 // - 리스트 아이템 배열 화면에 렌더링
+// - form에서 input[type="submit"] 시, 신규 리스트 아이템 추가
 // -----------------------------------------------------
 
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = ['숨쉬기'];
+
+// 리스트에 신규 아이템 추가
+function addItem(e) {
+  // console.log(this); // ? <form />
+  e.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  items.push(text); // 아이템 추가
+  populateList(items, itemsList); // 리스트 갱신
+  this.reset(); // form 초기화 (= input[name=item] value 비워줌)
+}
 
 // 리스트 화면 렌더링
 function populateList(plates = [], platesList) {
@@ -20,3 +31,4 @@ function populateList(plates = [], platesList) {
 }
 
 populateList(items, itemsList);
+addItems.addEventListener('submit', addItem);
