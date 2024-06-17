e9a2431a024ce0457753a5a6be0bd41170476b06
sryung <sryung1225@gmail.com>
2024-06-17 21:41:46 +0900

    [17][feature] typescript로 변환

--- 17_Sort_Without_Articles/script.js
+++ 17_Sort_Without_Articles/script.js
@@ -1,27 +1,22 @@
+"use strict";
 // 특정 단어 제외하고 배열 정렬
 // - json 데이터 fetch
 // - 정규표현식을 활용하여 문자열 내 관사 제거
 // - 문자열 알파벳 순 정렬
 // -----------------------------------------------------
-
 fetch('data.json')
-  .then((response) => response.json())
-  .then((data) => {
+    .then((response) => response.json())
+    .then((data) => {
     const bands = data.bands;
     console.log(bands);
-
     function strip(bandName) {
-      // 관사 a, the, an 제거
-      return bandName.replace(/^(a |the |an )/i, '').trim();
+        // 관사 a, the, an 제거
+        return bandName.replace(/^(a |the |an )/i, '').trim();
     }
-
-    const sortedBands = [...bands].sort((a, b) =>
-      // 알파벳 순 (a-z) 정렬
-      strip(a) > strip(b) ? 1 : -1,
-    );
+    const sortedBands = [...bands].sort((a, b) => 
+    // 알파벳 순 (a-z) 정렬
+    strip(a) > strip(b) ? 1 : -1);
     console.log(sortedBands);
-
-    document.querySelector('#bands').innerHTML = sortedBands
-      .map((band) => `<li>${band}</li>`)
-      .join('');
-  });
+    const bandList = document.querySelector('#bands');
+    bandList.innerHTML = sortedBands.map((band) => `<li>${band}</li>`).join('');
+});
---
+++ 17_Sort_Without_Articles/script.ts
@@ -0,0 +1,26 @@
+// 특정 단어 제외하고 배열 정렬
+// - json 데이터 fetch
+// - 정규표현식을 활용하여 문자열 내 관사 제거
+// - 문자열 알파벳 순 정렬
+// -----------------------------------------------------
+
+fetch('data.json')
+  .then((response) => response.json())
+  .then((data) => {
+    const bands: string[] = data.bands;
+    console.log(bands);
+
+    function strip(bandName: string) {
+      // 관사 a, the, an 제거
+      return bandName.replace(/^(a |the |an )/i, '').trim();
+    }
+
+    const sortedBands = [...bands].sort((a, b) =>
+      // 알파벳 순 (a-z) 정렬
+      strip(a) > strip(b) ? 1 : -1,
+    );
+    console.log(sortedBands);
+
+    const bandList = document.querySelector('#bands') as HTMLUListElement;
+    bandList.innerHTML = sortedBands.map((band) => `<li>${band}</li>`).join('');
+  });
