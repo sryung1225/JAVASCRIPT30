e88b3a726ecd02959d57faef3c301a4eb6069926
sryung <sryung1225@gmail.com>
2024-06-18 22:44:09 +0900

    [18][feature] 시간 합산 후 표현하기

---
+++ 18_Adding_Up_Times_with_Reduce/script.js
@@ -0,0 +1,23 @@
+// 시간 합산하고 표현하기
+// - 각 <li data-time="" /> 에 입력된 data-time 값 합산
+// - 합산된 초를 갖고 시/분/초 계산
+// -----------------------------------------------------
+
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+// 모든 timeNodes의 data-time에 입력된 초의 합산 계산
+const seconds = timeNodes
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [mins, secs] = timeCode.split(':').map(Number);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds, 0);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
