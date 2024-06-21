69ede3e792c0f1c8562e0e9c84c15de585b5de74
sryung <sryung1225@gmail.com>
2024-06-22 01:37:05 +0900

    [21][feature] typescript로 변환

--- 21_Geolocation/script.ts
+++ 21_Geolocation/script.ts
@@ -1,17 +1,20 @@
-"use strict";
 // 나침반 만들기
 // - 웹앱에서 사용자의 위치 파악을 지원하는 Geolocation API
 // - 사용자의 위치 변화를 지속적으로 추적 : watchPosition
 // -----------------------------------------------------
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
+const arrow = document.querySelector('.arrow') as HTMLDivElement;
+const speed = document.querySelector('.speed-value') as HTMLDivElement;
+
 // 현재 위치 파악
-navigator.geolocation.watchPosition((data) => {
+navigator.geolocation.watchPosition(
+  (data) => {
     console.log(data); // ? GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1719159387431}
     if (data.coords.speed) {
-        speed.textContent = `${data.coords.speed}`; // 사용자의 속도
+      speed.textContent = `${data.coords.speed}`; // 사용자의 속도
     }
     arrow.style.transform = `rotate(${data.coords.heading}deg)`; // 사용자의 방향 기준으로 낮침반 회전
-}, (error) => {
+  },
+  (error) => {
     console.error(error);
-});
+  },
+);
