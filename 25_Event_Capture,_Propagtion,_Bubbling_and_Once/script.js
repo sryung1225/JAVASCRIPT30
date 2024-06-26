0da2b9707fe8450e43349c1d979bd1e9807f9147
sryung <sryung1225@gmail.com>
2024-06-27 02:44:07 +0900

    [25][feature] 이벤트 버블링/캡쳐링 중지 - e.stopPropagation()을 사용하여 원하는 요소만 이벤트를 처리하도록 제어할 수 있음 - ex) 버블링 방식이라면 클릭된 단계의 요소 (one/two/three) - ex) 캡쳐링 방식이라면 최상위 요소 (one)

--- 25_Event_Capture,_Propagtion,_Bubbling_and_Once/script.js
+++ 25_Event_Capture,_Propagtion,_Bubbling_and_Once/script.js
@@ -2,21 +2,20 @@
 // - 중첩된 요소들 divs를 갖고 버블링과 캡쳐링을 이해
 // - 버블링: 이벤트가 가장 안쪽 요소에서 시작해서 최상위 요소로 전파되는 방식
 // - 캡쳐링: 이벤트가 최상위 요소에서 시작해서 가장 안쪽 요소로 전파되는 방식
+// - 버블링을 막고 원하는 요소만 이벤트를 하도록 제어 : e.stopPropagation()
 // -----------------------------------------------------
 const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
+  e.stopPropagation();
   // 가장 바깥(one)을 누르면 ? one
-  // 가장 안쪽(three)을 누르면 ? one two three
+  // 가장 안쪽(three)을 누르면 ? three
 }
 
-// div에 부여된 이벤트리스너가 캡쳐링 단계에서 실행하도록 조정 (capture: true)
+// div에 부여된 이벤트리스너가 버블링/캡쳐링 방식 선택 가능
 divs.forEach((div) =>
   div.addEventListener('click', logText, {
-    capture: true,
+    capture: false, // 버블링
   }),
 );
-
-// document.body.addEventListener('click', logText, { capture: true }));
-// 가장 안쪽(three)을 누르면 ? bod one two three
