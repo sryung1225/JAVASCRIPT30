99362142a888179f0068c98320fbcb612fa86530
sryung <sryung1225@gmail.com>
2024-06-27 02:50:58 +0900

    [25][feature] 이벤트 리스너가 1회만 실행되도록 제어 - once 옵션을 통해 이벤트 리스너가 한 번만 실행된 후 자동으로 제거되도록 설정 - 즉, 1회 logText()가 실행된 이후에는 더 이상 실행되지 않도록 함

--- 25_Event_Capture,_Propagtion,_Bubbling_and_Once/script.js
+++ 25_Event_Capture,_Propagtion,_Bubbling_and_Once/script.js
@@ -3,19 +3,22 @@
 // - 버블링: 이벤트가 가장 안쪽 요소에서 시작해서 최상위 요소로 전파되는 방식
 // - 캡쳐링: 이벤트가 최상위 요소에서 시작해서 가장 안쪽 요소로 전파되는 방식
 // - 버블링을 막고 원하는 요소만 이벤트를 하도록 제어 : e.stopPropagation()
+// - 이벤트 리스너가 한 번만 실행되도록 제어 : { once: true }
 // -----------------------------------------------------
 const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  e.stopPropagation();
   // 가장 바깥(one)을 누르면 ? one
-  // 가장 안쪽(three)을 누르면 ? three
+  // 그리고 가장 안쪽(three)을 누르면 ? three two
+
+  // e.stopPropagation(); // 이벤트 전파 중지
 }
 
 // div에 부여된 이벤트리스너가 버블링/캡쳐링 방식 선택 가능
 divs.forEach((div) =>
   div.addEventListener('click', logText, {
     capture: false, // 버블링
+    once: true, // 이벤트 리스너가 한 번 실행된 후 제거됨
   }),
 );
