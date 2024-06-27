112098b967c04f863810113a6529e1705be8e6dc
sryung <sryung1225@gmail.com>
2024-06-28 02:03:39 +0900

    [26][feature] 드롭다운 배경 애니메이션 적용 - 배경을 div.dropdown에 스타일링으로 주는 것이 아닌 별도의 요소 div.dropdownBackground로 주어 자유롭게 이동하도록 연출

--- 26_Stripe_Follow_Along_Nav/script.js
+++ 26_Stripe_Follow_Along_Nav/script.js
@@ -1,15 +1,40 @@
 // 마우스 hover와 드롭다운
 // - 각 메뉴에 해당하는 <a />에 마우스가 올라가면 형제인 div.dropdown를 노출
+// - 드롭다운의 배경을 별도의 요소로 지정 후 이동하는 듯한 애니메이션 연출
+// - 특정 요소의 크기/위치를 가져오는 getBoundingClientRect()
 // -----------------------------------------------------
 
 const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
 
 function handleEnter() {
   this.classList.add('trigger-enter');
+  background.classList.add('open');
+
+  // 드롭다운 배경의 위치/크기 지정 => 이동하는 듯한 애니메이션 연출
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect(); // 드롭다운의 크기/위치
+  const navCoords = nav.getBoundingClientRect(); // 네비게이션의 크기/위치
+
+  const coords = {
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+    width: dropdownCoords.width,
+    height: dropdownCoords.height,
+  };
+
+  background.style.setProperty(
+    'transform',
+    `translate(${coords.left}px, ${coords.top}px)`,
+  );
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
 }
 
 function handleLeave() {
   this.classList.remove('trigger-enter');
+  background.classList.remove('open');
 }
 
 triggers.forEach((trigger) =>
