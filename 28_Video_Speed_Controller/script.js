adc3078d4693b317d2a9f10328f690459e7a88fc
sryung <sryung1225@gmail.com>
2024-06-30 12:28:39 +0900

    [28][feature] typescript로 변환

--- 28_Video_Speed_Controller/script.js
+++ 28_Video_Speed_Controller/script.js
@@ -1,21 +1,22 @@
+"use strict";
 // 영상 재생 속도 컨트롤러
 // 게이지 바 안에서 마우스가 움직이는대로 영상 재생 속도를 조절
 // 11_Custom_Video_Player의 기능 중 하나
 // -----------------------------------------------------
-const speed = document.querySelector('.speed');
-const bar = speed.querySelector('.speed-bar');
-const video = document.querySelector('.flex');
-
-function handleMove(e) {
-  const y = e.pageY - this.offsetTop; // y 좌표
-  const percent = y / this.offsetHeight;
-  const min = 0.4;
-  const max = 4;
-  const height = Math.round(percent * 100) + '%';
-  const playbackRate = percent * (max - min) + min;
-  bar.style.height = height;
-  bar.textContent = playbackRate.toFixed(2) + '×'; // 소수점 아래 2자리로 고정
-  video.playbackRate = playbackRate; // 비디오 재생속도(playbackRate) 조절
+{
+    const speed = document.querySelector('.speed');
+    const bar = speed.querySelector('.speed-bar');
+    const video = document.querySelector('.flex');
+    function handleMove(e) {
+        const y = e.pageY - this.offsetTop; // y 좌표
+        const percent = y / this.offsetHeight;
+        const min = 0.4;
+        const max = 4;
+        const height = Math.round(percent * 100) + '%';
+        const playbackRate = percent * (max - min) + min;
+        bar.style.height = height;
+        bar.textContent = playbackRate.toFixed(2) + '×'; // 소수점 아래 2자리로 고정
+        video.playbackRate = playbackRate; // 비디오 재생속도(playbackRate) 조절
+    }
+    speed.addEventListener('mousemove', handleMove);
 }
-
-speed.addEventListener('mousemove', handleMove);
