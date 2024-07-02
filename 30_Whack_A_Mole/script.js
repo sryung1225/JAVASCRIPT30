751781e43323e4f726eb19b609d0f3ad1fa0c9e0
sryung <sryung1225@gmail.com>
2024-07-02 16:09:28 +0900

    [30][init] 초기 셋팅 - html + style + script

---
+++ 30_Whack_A_Mole/script.js
@@ -0,0 +1,5 @@
+// 두더지 잡기 게임
+// -----------------------------------------------------
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
