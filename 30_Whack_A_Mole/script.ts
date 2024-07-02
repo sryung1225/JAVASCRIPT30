c74ad3937a7e6e6f4d82f3430c4a4f4db37a5b98
sryung <sryung1225@gmail.com>
2024-07-02 16:57:13 +0900

    [30][feature] typescript로 변환

---
+++ 30_Whack_A_Mole/script.ts
@@ -0,0 +1,65 @@
+// 두더지 잡기 게임
+// - 한 판당 10초간 진행
+// - 각 구멍에는 한 마리의 두더지가 배치
+// - 두더지는 한 마리씩, 매번 다른 구멍(randomHole), 매번 다른 시간동안(randomTime) 등장(peep)
+// - 두더지가 등장했을 때 클릭하면 점수 획득
+// -----------------------------------------------------
+const holes = document.querySelectorAll('.hole') as NodeListOf<HTMLDivElement>; // 구멍
+const scoreBoard = document.querySelector('.score') as HTMLSpanElement;
+const moles = document.querySelectorAll('.mole') as NodeListOf<HTMLDivElement>; // 두더지
+
+let lastHole: HTMLDivElement; // 가장 최근 두더지가 나왔던 구멍 : 연속 등장 방어
+let timeUp: boolean = false; // 게임 중 여부
+let score: number = 0; // 점수
+
+// min~max 내 랜덤 시간 생성 후 반환
+function randomTime(min: number, max: number) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// 두더지가 등장할 구멍 랜덤 지정
+function randomHole(holes: NodeListOf<HTMLDivElement>) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  // 한 구멍에서 연속으로 두더지가 등장하지 않도록 제어
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+// 랜덤 두더지 등장
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add('up'); // 지정된 구멍에 스타일 추가 : 두더지 등장
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) peep(); // 잔여 시간이 남아있으면 랜덤 두더지 재등장
+  }, time); // 일정 시간 이후 두더지 숨기기
+}
+
+// 게임 시작
+function startGame() {
+  // 설정 초기화
+  scoreBoard.textContent = `0`;
+  timeUp = false;
+  score = 0;
+  // 두더지 첫 등장
+  peep();
+  setTimeout(() => (timeUp = true), 10000); // 게임은 10초간 진행
+}
+
+// 두더지 잡기
+function bonk(this: HTMLDivElement, e: MouseEvent) {
+  if (!e.isTrusted) return;
+
+  const parent = this.parentNode as HTMLElement;
+  if (!parent) return;
+
+  score++; // 점수 획득
+  parent.classList.remove('up'); // 두더지 숨기기
+  scoreBoard.textContent = `${score}`;
+}
+moles.forEach((mole) => mole.addEventListener('click', bonk));
